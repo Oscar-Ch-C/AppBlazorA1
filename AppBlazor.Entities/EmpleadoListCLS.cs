517940adb5825bed6f2f4d7a1e30f3883105b408
Alumno <Alumno@b403-15>
2025-08-23 10:08:41 -0400

    hoy1

---
+++ AppBlazor.Entities/EmpleadoListCLS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBlazor.Entities
+{
+    public class EmpleadoListCLS
+    {
+        public int Num_empleado { get; set; }
+        public int edad { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Cargo { get; set; } = null!;
+        public DateTime fechaEmpleado { get; set; }
+        public int Cuota { get; set; }
+        public int Ventas { get; set; }
+
+    }
+}
